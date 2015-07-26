86b9d1ec19e7651aeff0cba9a13ea7530fb07aeb
Nick Riebeek <nick.riebeek@gmail.com>
2015-07-26 15:29:17 -0400

    updated publication check

--- server/publications.js
+++ server/publications.js
@@ -1,14 +1,14 @@
 Meteor.publish('customers', function(skipCount) {
-  Counts.publish(this, 'customerCount', Customers.find(), { 
-    noReady: true
-  });
-  
   var positiveIntegerCheck = Match.Where(function(x) {
     check(x, Match.Integer);
     return x >= 0;
   });
   check(skipCount, positiveIntegerCheck);
-
+  
+  Counts.publish(this, 'customerCount', Customers.find(), { 
+    noReady: true
+  });
+  
   return Customers.find({}, {
     limit: parseInt(Meteor.settings.public.recordsPerPage),
     skip: skipCount
