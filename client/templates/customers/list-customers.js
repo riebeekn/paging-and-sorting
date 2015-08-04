d53245bac41709a47088463a2b825b3b70f570b9
Nick Riebeek <nick.riebeek@gmail.com>
2015-08-04 12:21:26 -0400

    part 1 implementation

--- client/templates/customers/list-customers.js
+++ client/templates/customers/list-customers.js
@@ -1,13 +1,45 @@
+Template.listCustomers.onCreated(function() {
+  var template = this;
+
+  template.autorun(function() {
+    var skipCount = (currentPage() - 1) * Meteor.settings.public.recordsPerPage;
+    template.subscribe('customers', skipCount);
+  });
+});
+
 Template.listCustomers.helpers({
-	customers: function() {
-		return Customers.find();
-	}
+  customers: function() {
+    return Customers.find();
+  },
+  prevPage: function() {
+    var previousPage = currentPage() === 1 ? 1 : currentPage() - 1;
+    return Router.routes.listCustomers.path({page: previousPage});
+  },
+  nextPage: function() {
+    var nextPage = hasMorePages() ? currentPage() + 1 : currentPage();
+    return Router.routes.listCustomers.path({page: nextPage});
+  },
+  prevPageClass: function() {
+    return currentPage() <= 1 ? "disabled" : "";
+  },
+  nextPageClass: function() {
+    return hasMorePages() ? "" : "disabled";
+  }
 });
 
 Template.listCustomers.events({
-	'click #btnAddCustomer': function(e) {
-		e.preventDefault();
+  'click #btnAddCustomer': function(e) {
+    e.preventDefault();
 
-		Router.go('addCustomer');
-	}
+    Router.go('addCustomer');
+  }
 });
+
+var hasMorePages = function() {
+  var totalCustomers = Counts.get('customerCount');
+  return currentPage() * parseInt(Meteor.settings.public.recordsPerPage) < totalCustomers;
+}
+
+var currentPage = function() {
+  return parseInt(Router.current().params.page) || 1; 
+}
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,16 @@
-Meteor.publish('customers', function() {
-	return Customers.find();
+Meteor.publish('customers', function(skipCount) {
+  var positiveIntegerCheck = Match.Where(function(x) {
+    check(x, Match.Integer);
+    return x >= 0;
+  });
+  check(skipCount, positiveIntegerCheck);
+
+  Counts.publish(this, 'customerCount', Customers.find(), { 
+    noReady: true
+  });
+  
+  return Customers.find({}, {
+    limit: parseInt(Meteor.settings.public.recordsPerPage),
+    skip: skipCount
+  });
 });
